2b0c7a0e26f8b2c2fb7099af00ae6f6e1bec43df
Teguhriyadi <ilham.teguh55@gmail.com>
2023-02-15 10:40:04 +0700

    Update BUG API Role

--- app/Http/Controllers/API/Master/RoleController.php
+++ app/Http/Controllers/API/Master/RoleController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\API\Master;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Master\Role\GetRoleResource;
 use App\Models\Master\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class RoleController extends Controller
 {
+    public function index()
+    {
+        return DB::transaction(function () {
+            $role = Role::orderBy("created_at", "DESC")->get();
+
+            return GetRoleResource::collection($role);
+        });
+    }
+
     public function update(Request $request, $id_role)
     {
         return DB::transaction(function () use ($id_role, $request) {
