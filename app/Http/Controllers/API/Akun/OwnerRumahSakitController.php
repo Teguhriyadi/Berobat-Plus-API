880c0f079910b2a1103508d47ec0eb26a35d2632
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-05-05 14:52:42 +0700

    Add Migrations and Model Owner Rumah Sakit

---
+++ app/Http/Controllers/API/Akun/OwnerRumahSakitController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\Akun;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OwnerRumahSakitController extends Controller
+{
+    //
+}
---
+++ app/Models/Akun/OwnerRumahSakit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Akun;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OwnerRumahSakit extends Model
+{
+    use HasFactory;
+
+    protected $table = "owner_rumah_sakit";
+
+    protected $guarded = [''];
+
+    public $primaryKey = "id_owner_rumah_sakit";
+
+    protected $keyType = "string";
+
+    public $incrementing = false;
+}
