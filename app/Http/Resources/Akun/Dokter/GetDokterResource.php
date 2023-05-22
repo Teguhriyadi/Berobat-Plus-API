f99048a65842a84bc673a040310f3ea29b48a525
Teguhriyadi <ilham.teguh55@gmail.com>
2023-05-22 20:29:31 +0700

    Upload

--- app/Http/Resources/Akun/Dokter/GetDokterResource.php
+++ app/Http/Resources/Akun/Dokter/GetDokterResource.php
@@ -20,6 +20,7 @@ class GetDokterResource extends JsonResource
             "nomor_str" => $this->nomor_str,
             "kelas" => $this->kelas,
             "biaya" => $this->getBiaya,
+            "harga" => "Rp. " . number_format($this->getBiaya->biaya)
         ];
     }
 }
