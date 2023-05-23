6d37e234f4ce099b1bf9253d12127d4d02a62bd3
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-05-23 15:42:40 +0700

    Update Status

--- app/Http/Controllers/API/Akun/Public/ActivateAccountController.php
+++ app/Http/Controllers/API/Akun/Public/ActivateAccountController.php
@@ -14,15 +14,11 @@ class ActivateAccountController extends Controller
     {
         return DB::transaction(function () use ($id_user) {
 
-            if (Auth::user()->status == "0") {
-                $status = "1";
-            } else if (Auth::user()->status == "1") {
-                $status = "0";
-            }
+            $cek = User::where("id", $id_user)->first();
 
             User::where("id", $id_user)->update([
                 "created_by" => Auth::user()->id,
-                "status" => $status
+                "status" => $cek->status == "1" ? "0" : "1"
             ]);
 
             return response()->json(["pesan" => "Status Akun Berhasil Diubah"]);
