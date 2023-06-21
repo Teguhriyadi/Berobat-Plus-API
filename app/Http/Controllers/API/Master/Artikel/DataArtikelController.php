b20c5463fe078e808283ed37d7336685cb149c4c
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-06-21 18:49:06 +0700

    push

--- app/Http/Controllers/API/Master/Artikel/DataArtikelController.php
+++ app/Http/Controllers/API/Master/Artikel/DataArtikelController.php
@@ -21,7 +21,7 @@ class DataArtikelController extends Controller
     public function index()
     {
         return DB::transaction(function () {
-            $artikel = DataArtikel::get();
+            $artikel = DataArtikel::orderBy(DB::raw("RAND()"))->with("getUser:id,nama")->paginate(4);
 
             return GetArtikelResource::collection($artikel);
         });
