dc61e288cbb9a0fd6186ecb97bf78842d2146464
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-06-21 16:36:22 +0700

    push

--- database/seeders/Master/Penyakit/SpesialisPenyakitSeeder.php
+++ database/seeders/Master/Penyakit/SpesialisPenyakitSeeder.php
@@ -19,70 +19,70 @@ class SpesialisPenyakitSeeder extends Seeder
             "id_spesialis_penyakit" => "SPS-001",
             "nama_spesialis" => "Dentist",
             "slug_spesialis" => "dentist",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-tooth"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-002",
             "nama_spesialis" => "THT",
             "slug_spesialis" => "tht",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-ear-listen"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-003",
             "nama_spesialis" => "Orthopedi",
             "slug_spesialis" => "orthopedi",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-bone"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-004",
             "nama_spesialis" => "Jantung",
             "slug_spesialis" => "jantung",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-heart-pulse"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-005",
             "nama_spesialis" => "Penyakit-dalam",
             "slug_spesialis" => "penyakit-dalam",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-hand-holding-heart"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-006",
             "nama_spesialis" => "Kulit",
             "slug_spesialis" => "kulit",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-hand-dots"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-007",
             "nama_spesialis" => "Mata",
             "slug_spesialis" => "mata",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-eye"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-008",
             "nama_spesialis" => "Neurologist",
             "slug_spesialis" => "neurologist",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-lungs-virus"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-009",
             "nama_spesialis" => "Internist",
             "slug_spesialis" => "internist",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-person-breastfeeding"
         ]);
 
         SpesialisPenyakit::create([
             "id_spesialis_penyakit" => "SPS-010",
             "nama_spesialis" => "Obstetrics",
             "slug_spesialis" => "obstetics",
-            "icon" => "fas fa-user"
+            "icon" => "fa-solid fa-head-side-mask"
         ]);
     }
 }
