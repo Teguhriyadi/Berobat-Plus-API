345aae0f79fce1ee48f96f2827ed130373351652
Teguhriyadi <ilham.teguh55@gmail.com>
2023-07-09 01:30:18 +0700

    Update Valdiasi

--- app/Http/Controllers/API/Master/Ahli/JadwalPraktekController.php
+++ app/Http/Controllers/API/Master/Ahli/JadwalPraktekController.php
@@ -7,8 +7,11 @@ use App\Http\Resources\Master\Ahli\GetJadwalPraktekResource;
 use App\Models\Ahli\DetailPraktek;
 use App\Models\Ahli\JadwalPraktek;
 use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class JadwalPraktekController extends Controller
 {
@@ -25,39 +28,53 @@ class JadwalPraktekController extends Controller
     
     public function store(Request $request, $id_detail_praktek)
     {
-        return DB::transaction(function() use($request, $id_detail_praktek) {
-            $hari = $request["hari"];
-            
-            $startDate = Carbon::now()->startOfMonth();
-            $endDate = Carbon::now()->endOfMonth();
-            
-            $currentDate = $startDate->copy();
-            
-            if (JadwalPraktek::where('hari', $hari)
-            ->whereBetween('tanggal', [$startDate->format('Y-m-d'), $endDate->format('Y-m-d')])
-            ->exists()
-            ) {
-                return response()->json(["pesan" => "Data Jadwal Praktek Dengan Hari {$hari} Sudah Tersedia Pada Bulan Ini."], 400);
-            }
-            
-            $nomer = 0;
-            while ($currentDate->lte($endDate) && $currentDate->month === $startDate->month) {
-                if ($currentDate->format('l') === $hari && $currentDate->gte(Carbon::now()->startOfDay())) {
-                    $jadwal = new JadwalPraktek();
-                    $jadwal->id_jadwal_praktek = "JDWL-P-" . date("YmdHis") . $nomer++;
-                    $jadwal->id_detail_praktek = $id_detail_praktek;
-                    $jadwal->hari = $hari;
-                    $jadwal->tanggal = $currentDate->format('Y-m-d');
-                    $jadwal->mulai_jam = $request["mulai_jam"];
-                    $jadwal->selesai_jam = $request["selesai_jam"];
-                    $jadwal->save();
+        try {
+            $messages = [
+                "required" => "Kolom :attribute Harus Diisi"
+            ];
+    
+            $this->validate($request, [
+                "hari" => "required",
+                "mulai_jam" => "required",
+                "selesai_jam" => "required"
+            ], $messages);
+    
+            return DB::transaction(function() use($request, $id_detail_praktek) {
+                $hari = $request["hari"];
+                
+                $startDate = Carbon::now()->startOfMonth();
+                $endDate = Carbon::now()->endOfMonth();
+                
+                $currentDate = $startDate->copy();
+                
+                if (JadwalPraktek::where('hari', $hari)
+                ->whereBetween('tanggal', [$startDate->format('Y-m-d'), $endDate->format('Y-m-d')])
+                ->exists()
+                ) {
+                    return response()->json(["pesan" => "Data Jadwal Praktek Dengan Hari {$hari} Sudah Tersedia Pada Bulan Ini."], 400);
                 }
                 
-                $currentDate->addDay();
-            }
-            
-            return response()->json(["pesan" => "Data Berhasil di Tambahkan"]);
-        });
+                $nomer = 0;
+                while ($currentDate->lte($endDate) && $currentDate->month === $startDate->month) {
+                    if ($currentDate->format('l') === $hari && $currentDate->gte(Carbon::now()->startOfDay())) {
+                        $jadwal = new JadwalPraktek();
+                        $jadwal->id_jadwal_praktek = "JDWL-P-" . date("YmdHis") . $nomer++;
+                        $jadwal->id_detail_praktek = $id_detail_praktek;
+                        $jadwal->hari = $hari;
+                        $jadwal->tanggal = $currentDate->format('Y-m-d');
+                        $jadwal->mulai_jam = $request["mulai_jam"];
+                        $jadwal->selesai_jam = $request["selesai_jam"];
+                        $jadwal->save();
+                    }
+                    
+                    $currentDate->addDay();
+                }
+                
+                return response()->json(["pesan" => "Data Berhasil di Tambahkan"]);
+            });
+        } catch (ValidationException $e) {
+            return response()->json(["errors" => $e->errors()], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+        }
     }
     
     public function edit($id_jadwal_praktek)
