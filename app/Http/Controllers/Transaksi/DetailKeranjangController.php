5b6724ef8539e4947f234c25c4be193a5393f513
Teguhriyadi <ilham.teguh55@gmail.com>
2023-07-18 00:56:52 +0700

    Update

--- app/Http/Controllers/Transaksi/DetailKeranjangController.php
+++ app/Http/Controllers/Transaksi/DetailKeranjangController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Transaksi;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apotek\Produk\ProdukApotek;
+use App\Models\Master\Obat\TransaksiObat;
 use App\Models\Transaksi\Keranjang;
 use App\Models\Transaksi\KeranjangDetail;
 use Illuminate\Http\Request;
@@ -71,7 +72,24 @@ class DetailKeranjangController extends Controller
     public function tambah($id_keranjang_detail)
     {
         return DB::transaction(function() use ($id_keranjang_detail) {
-            echo $id_keranjang_detail;
+            $detail = KeranjangDetail::where("id_keranjang_detail", $id_keranjang_detail)->first();
+            
+            $produk = ProdukApotek::where("id_produk", $detail->produk_id)->first();
+            
+            $detail->update([
+                "jumlah" => $detail->jumlah + 1,
+                "jumlah_harga" => $detail->jumlah_harga + $produk->harga_produk
+            ]);
+
+            $jumlah_harga_keranjang = KeranjangDetail::where("keranjang_id", $detail->keranjang_id)->sum("jumlah_harga");
+
+            $keranjang = Keranjang::where("id_keranjang", $detail->keranjang_id)->first();
+
+            $keranjang->update([
+                "jumlah_harga" => $jumlah_harga_keranjang
+            ]);
+            
+            return response()->json(["pesan" => "Produk Berhasil di Tambahkan"]);
         });
     }
 }
