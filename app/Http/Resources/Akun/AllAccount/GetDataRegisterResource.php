5e0ce9c2ea0f83aeb9313e305f04014d512e73d3
Teguhriyadi <ilham.teguh55@gmail.com>
2023-07-28 18:00:04 +0700

    Update

--- app/Http/Resources/Akun/AllAccount/GetDataRegisterResource.php
+++ app/Http/Resources/Akun/AllAccount/GetDataRegisterResource.php
@@ -14,13 +14,31 @@ class GetDataRegisterResource extends JsonResource
      */
     public function toArray($request)
     {
+        if ($this->getRole->id_role == "RO-2003062") {
+
+            $file_dokumen = $this->getDokter->file_dokumen;
+
+        } else if ($this->getRole->id_role == "RO-2003063") {
+
+            $file_dokumen = $this->getPerawat->file_dokumen;
+
+        } else if ($this->getRole->id_role == "RO-2003065") {
+
+            $file_dokumen = $this->getApotek->file_dokumen;
+
+        } else if ($this->getRole->id_role == "RO-2003066") {
+
+            $file_dokumen = $this->getOwnerRs->file_dokumen;
+            
+        }
         return [
             "id" => $this->id,
             "nama" => $this->nama,
             "nomor_hp" => $this->nomor_hp,
             "alamat" => $this->alamat,
             "role" => $this->getRole->only("id_role", "nama_role"),
-            "foto" => $this->foto
+            "foto" => $this->foto,
+            "file_dokumen" => $file_dokumen
         ];
     }
 }
