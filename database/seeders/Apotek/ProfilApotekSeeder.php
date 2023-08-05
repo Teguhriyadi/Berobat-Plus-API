9f4f667cb4c3fe630d6937a1a8e335509eb51200
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-08-05 14:41:43 +0700

    push

--- database/seeders/Apotek/ProfilApotekSeeder.php
+++ database/seeders/Apotek/ProfilApotekSeeder.php
@@ -21,9 +21,10 @@ class ProfilApotekSeeder extends Seeder
             "slug_apotek" => "apotek-kimia-farma-arjawinangun",
             "deskripsi_apotek" => "Bandung",
             "alamat_apotek" => "Jakarta Raya",
-            "nomor_hp" => "2389283923",
+            "nomor_hp_apotek" => "2389283923",
             "status" => 1,
             "id_user" => 8,
+            "user_penanggung_jawab_id" => 8,
             "latitude" => "-6.6539487",
             "longitude" => "108.4072596"
         ]);
@@ -34,9 +35,10 @@ class ProfilApotekSeeder extends Seeder
             "slug_apotek" => "apotek-kimia-farma-klayan",
             "deskripsi_apotek" => "Bandung Jakarta",
             "alamat_apotek" => "Jakarta Raya",
-            "nomor_hp" => "2389283924",
+            "nomor_hp_apotek" => "2389283924",
             "status" => 0,
             "id_user" => 8,
+            "user_penanggung_jawab_id" => 8,
             "latitude" => "-6.6868722",
             "longitude" => "108.5462849"
         ]);
