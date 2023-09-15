2eea65461a8b4cd0fcc6e8eb38a69ed9619ea5f3
Teguhriyadi <ilham.teguh55@gmail.com>
2023-09-15 13:55:15 +0700

    Update

--- app/Http/Controllers/API/Akun/KonsumenController.php
+++ app/Http/Controllers/API/Akun/KonsumenController.php
@@ -16,15 +16,15 @@ class KonsumenController extends Controller
     {
         return DB::transaction(function () {
             $konsumen = Konsumen::orderBy("created_at", "DESC")->paginate(10);
-
+            
             return GetKonsumenResource::collection($konsumen);
         });
     }
-
+    
     public function store(ValidatorKonsumen $request)
     {
         return DB::transaction(function () use ($request) {
-
+            
             $user = User::create([
                 "nama" => $request->nama,
                 "password" => bcrypt($request->password),
@@ -34,13 +34,37 @@ class KonsumenController extends Controller
                 "id_role" => "RO-2003064",
                 "status" => 1
             ]);
-
+            
             $konsumen = Konsumen::create([
                 "id_konsumen" => "KSN-" . date("YmdHis"),
                 "user_id" => $user->id,
                 "nik" => $request->nik
             ]);
-
+            
+            $curl = curl_init();
+            
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.fonnte.com/send',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'target' => $request->nomor_hp|$request->nama,
+                    'message' => 'Hamdan',
+                ),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: ' . env("TOKEN_WA ") 
+                ),
+            ));
+            
+            $response = curl_exec($curl);
+            
+            curl_close($curl);
+            
             return response()->json([
                 "pesan" => "Data Konsumen Berhasil di Tambahkan", 
                 "user" => $user["id"],
@@ -48,22 +72,22 @@ class KonsumenController extends Controller
             ]);
         });
     }
-
+    
     public function edit($id_konsumen)
     {
         return DB::transaction(function () use ($id_konsumen) {
             $konsumen = Konsumen::where("id_konsumen", $id_konsumen)->first();
-
+            
             return new GetKonsumenResource($konsumen);
         });
     }
-
+    
     public function update(Request $request, $id_konsumen)
     {
         return DB::transaction(function () use ($id_konsumen, $request) {
-
+            
             $konsumen = Konsumen::where("id_konsumen", $id_konsumen)->first();
-
+            
             User::where("id", $konsumen->user_id)->update([
                 "nama" => $request->nama,
                 "email" => $request->email,
@@ -76,21 +100,21 @@ class KonsumenController extends Controller
                 "tempat_lahir" => $request->tempat_lahir,
                 "tanggal_lahir" => $request->tanggal_lahir
             ]);
-
+            
             Konsumen::where("id_konsumen", $id_konsumen)->update([
                 "nik" => $request->nik
             ]);
-
+            
             return response()->json(["pesan" => "Data Konsumen Berhasil di Simpan"]);
         });
     }
-
+    
     public function destroy($id_konsumen)
     {
         return DB::transaction(function () use ($id_konsumen) {
-
+            
             Konsumen::where("id_konsumen", $id_konsumen)->delete();
-
+            
             return response()->json(["pesan" => "Data Konsumen Berhasil di Hapus"]);
         });
     }
